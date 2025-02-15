891950d46f46921fb6c8f9439b8c011a470cc96a
oe154 <o.ebada1@uni.brighton.ac.uk>
2025-02-15 17:26:49 +0000

    Setting up news files

---
+++ app/src/main/java/com/example/blushfinance/NewsPage/News_Page.java
@@ -0,0 +1,27 @@
+package com.example.blushfinance.NewsPage;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.blushfinance.R;
+
+public class News_Page extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_news);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.news), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+
+}
