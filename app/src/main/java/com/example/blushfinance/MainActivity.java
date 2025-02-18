e1366af1b92061ee21f87df78a0e88069a8a4f06
PraiseBeTheLord <josephhealy2509@gmail.com>
2025-02-18 18:05:03 +0000

    we are still goated

--- app/src/main/java/com/example/blushfinance/MainActivity.java
+++ app/src/main/java/com/example/blushfinance/MainActivity.java
@@ -4,11 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import com.example.blushfinance.fragments.FinanceFragment;
-import com.example.blushfinance.fragments.HomeFragment;
-import com.example.blushfinance.fragments.NewsFragment;
-import com.example.blushfinance.fragments.ProfileFragment;
-import com.example.blushfinance.fragments.PotsFragment;
+import com.example.blushfinance.fragments.Finance_Page.FinanceFragment;
+import com.example.blushfinance.fragments.Home_Page.HomeFragment;
+import com.example.blushfinance.fragments.News_Page.NewsFragment;
+import com.example.blushfinance.fragments.Profile_Page.ProfileFragment;
+import com.example.blushfinance.fragments.Pots_Page.PotsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
