30d1e11ed5cf79275e2a4d611c2942c27c962869
bilal Elsamad <bilalsamad453@gmail.com>
2025-03-06 12:21:02 +0000

    Updating Main Act for login logic

--- app/src/main/java/com/example/blushfinance/MainActivity.java
+++ app/src/main/java/com/example/blushfinance/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.blushfinance;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -12,21 +17,46 @@ import com.example.blushfinance.fragments.Pots_Page.PotsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextUsername, editTextPassword;
+    private Button buttonLogin;
+    private boolean isLoggedIn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
 
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editTextUsername.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+
+                if (username.equals("1234") && password.equals("group")) {
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    isLoggedIn = true;
+                    showMainApp();
+                } else {
+                    Toast.makeText(MainActivity.this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void showMainApp() {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav_menu);
         bottomNav.setSelectedItemId(R.id.nav_home);
         bottomNav.setOnItemSelectedListener(navListener);
 
-        // Load default fragment (Home)
-        if (savedInstanceState == null) {
+        if (isLoggedIn) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container, new HomeFragment())
                     .commit();
@@ -54,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
                             .commit();
                     return true;
                 }
-
                 return false;
             };
 }
+
